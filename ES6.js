8ee3adb22d9f002a8747014454325c6e0d5e6903
yahjanus <yahjanus@gmail.com>
2022-05-09 11:21:14 +0900

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -1,1 +1,11 @@
-var a=5
+var a = 5;
+
+for (var i = 0; i < 5; i++) {
+  console.log("안쪽" + i);
+}
+console.log("바깥쪽" + i); //  함수단위 Scope로 정상 출력.
+
+for (let j = 0; j < 5; j++) {
+  console.log("안쪽" + j);
+}
+console.log("바깥쪽" + j); // 오류 발생. let으로 선언한 j는 블록단위 Scope로 {} 안에서만 유효.
